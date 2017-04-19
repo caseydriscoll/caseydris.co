c6732087a58ac9c988d8260c66558b69a40feeb2
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2017-04-18 21:31:38 -0400

    Add initial branding colors and spacing

--- web/app/themes/casey/resources/views/partials/footer.blade.php
+++ web/app/themes/casey/resources/views/partials/footer.blade.php
@@ -2,4 +2,6 @@
   <div class="container">
     @php(dynamic_sidebar('sidebar-footer'))
   </div>
+  <script src="https://use.typekit.net/gqo6guv.js"></script>
+  <script>try{Typekit.load({ async: true });}catch(e){}</script>
 </footer>
